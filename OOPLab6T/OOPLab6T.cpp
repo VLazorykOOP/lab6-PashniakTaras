4bcf752e0b841bb532f78ae7c285367e333b7b48
Taras Pashniak <taraspashnyak93@gmail.com>
2023-05-22 19:49:34 +0300

    Виправляю код

--- OOPLab6T/OOPLab6T.cpp
+++ OOPLab6T/OOPLab6T.cpp
@@ -14,16 +14,17 @@ T minElement(T* arr, int size)
     return minElem;
 }
 
-template<>
-char minElement(char* arr, int size)
-{
-    char minElem = arr[0];
-    for (int i = 1; i < size; i++)
-        if (arr[i] < minElem)
+template <>
+char* minElement<char*>(char** arr, int size) {
+    char* minElem = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (strcmp(arr[i], minElem) < 0) {
             minElem = arr[i];
-
+        }
+    }
     return minElem;
 }
+
 void Task1Char()
 {
     int size = 0;
